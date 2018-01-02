92529a0bf8230ffaf8940e7c24087c3533ea8e8d
Rudy Gomez <rgomez@gmail.com>
2018-01-02 00:43:38 -0800

    added utilities file

---
+++ FlickrFeed/FlickrFeed/Utilities.m
@@ -0,0 +1,13 @@
+//
+//  Utilities.m
+//  FlickrFeed
+//
+//  Created by Rudy Gomez on 12/31/17.
+//  Copyright © 2017 JRudy Gaming. All rights reserved.
+//
+
+#import "Utilities.h"
+
+@implementation Utilities
+
+@end
